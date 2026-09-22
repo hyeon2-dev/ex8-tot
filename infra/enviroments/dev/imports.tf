0587f1a0a6e2a73640b396192719f29133fda051
std19 <ski0123@naver.com>
2026-09-22 09:35:17 +0900

    인프라 구축 및 배포 확인

--- infra/enviroments/dev/imports.tf
+++ infra/enviroments/dev/imports.tf
@@ -1,4 +1,4 @@
-import {
-  to = module.eks.aws_eks_access_entry.bipa17-student19
-  id = "std19-ex8-eks-cluster:arn:aws:iam::925047940866:user/bipa17-student19"
-}
+# import {
+#   to = module.eks.aws_eks_access_entry.bipa17-student19
+#   id = "std19-ex8-eks-cluster:arn:aws:iam::925047940866:user/bipa17-student19"
+# }
